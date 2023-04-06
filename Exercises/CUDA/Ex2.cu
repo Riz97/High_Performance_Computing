89284167bbddb51e48da29cf4bc59c3aa1aff9af
Riccardo Caprile <93477707+Riz97@users.noreply.github.com>
2023-04-06 21:00:14 +0200

    Add files via upload

--- Exercises/CUDA/Ex2.cu
+++ Exercises/CUDA/Ex2.cu
@@ -2,10 +2,10 @@
 
 
 //Number of blocks , number of threads per block
-//<<<1,1>>>
-//<<<1,10>>>
-//<<<10,1>>>
-//<<<10,10>>>
+//<<<1,1>>> print This is running in parallel. one time
+//<<<1,10>>> print this is running in parallel ten times
+//<<<10,1>>> print this is running in parallel ten times
+//<<<10,10>>> print this is running in parallel one hundred times , 10 threads per 10 blocks = 100 execution of that line
 
 
 
@@ -17,6 +17,6 @@ __global__ void GPUFunction()
 
 int main()
 {
-    GPUFunction <<<5,5>>>();
-    cudaDeviceSynchronization();
+    GPUFunction <<<10,10>>>();
+      cudaDeviceSynchronize();
 }
